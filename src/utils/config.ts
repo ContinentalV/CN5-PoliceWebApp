cba7189a4f45f6a52996a65e3f26568e6ff2af5d
Neo-Getsu <damien.devinfo@gmail.com>
2024-03-18 16:36:36 +0100

    Issue p^roduction authentication

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,6 +1,6 @@
 // config.ts
 export const DISCORD_CLIENT: string = "1192220732277674134";
-export const DISCORD_REDIRECT: string = "https://policebot.vibrant-darwin.37-60-246-29.plesk.page:3000/redirectPage";
+export const DISCORD_REDIRECT: string = "https://www.policeapp.vibrant-darwin.37-60-246-29.plesk.page:3000/redirectPage";
 export const DISCORD_SCOPE: string = "identify+email+guilds.members.read";
 export const DISCORD_SECRET: string = "mvVq6ZYsrlaPNz7kB9JQqDNuO_3O7w68";
 export const BCSOHG = "1147211942797262945";
